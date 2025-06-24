f50fc135da9b00e5f370acd63d8938baadc569a5
Felipe-Alves-Augusto <70119937+Felipe-Alves-Augusto@users.noreply.github.com>
2025-06-23 23:28:52 -0300

    chore: section hero update

--- src/components/HomeComponents/HeroSection/index.tsx
+++ src/components/HomeComponents/HeroSection/index.tsx
@@ -20,7 +20,41 @@ const HeroSection = () => {
       </div>
 
       <Container>
-        <div className="flex items-center flex-col sm:flex-row justify-center mt-12 gap-10 sm:gap-0 mb-20">
+        <div className="flex flex-col text-[#404040]/80 gap-5">
+          <p>
+            <strong>A verdade é uma só:</strong>
+          </p>
+          <p>Se você vai casar (ou pretende casar um dia)…</p>
+          <p>
+            Se você é mãe, madrinha, amiga ou aquela mulher que gosta de estar
+            preparada para ajudar…
+          </p>
+          <p>Você simplesmente NÃO PODE ficar sem isso.</p>
+          <p>
+            Porque deixa eu te contar: casamento sem organização, sem estratégia
+            e sem clareza, é sinônimo de quê?
+          </p>
+          <p>
+            Caos. Ansiedade. Desespero. Escolhas erradas. Desperdício. E, muitas
+            vezes, frustração.
+          </p>
+          <p>
+            E aqui, a gente não aceita menos do que um dia absolutamente
+            incrível, leve, inteligente e… claro, chiquérrimo.
+          </p>
+          <p>
+            Por isso, nasceu o <strong>CASAMENTO CHIC PAGANDO POUCO —</strong>
+          </p>
+          <p>
+      
+            Um guia prático, elegante, sofisticado e super objetivo para
+            destravar sua organização, te ensinar como fazer escolhas
+            inteligentes, otimizar seus recursos e viver o seu sonho sem
+            estresse, sem desperdício e sem aquele drama todo que você vê por
+            aí.
+          </p>
+        </div>
+        {/* <div className="flex items-center flex-col sm:flex-row justify-center mt-12 gap-10 sm:gap-0 mb-20">
           <CardHero
             title="PLANEJAMENTO"
             description="Ajudamos você a planejar cada detalhe do seu casamento, desde a escolha do local até os mínimos detalhes da decoração."
@@ -41,7 +75,7 @@ const HeroSection = () => {
           >
             <Paperclip className="text-app-primary size-5" />
           </CardHero>
-        </div>
+        </div> */}
       </Container>
     </div>
 
